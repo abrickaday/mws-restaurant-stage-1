cbcaf1b88494c54f3c20ad0591d89c9bce6954a2
HJ <hongjinlau@gmail.com>
2019-02-26 17:56:17 +0800

    Fix: Fix alt attributes for images

--- js/main.js
+++ js/main.js
@@ -173,7 +173,7 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  image.alt = restaurant.name;
+  image.alt = `Photo of ${restaurant.name} Restaurant`;
   li.append(image);
 
   const name = document.createElement('h1');
