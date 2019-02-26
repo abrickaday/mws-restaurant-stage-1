d12bff61276e7ee87670efa20f6ec01c9dcb5b10
HJ <hongjinlau@gmail.com>
2019-02-26 18:24:28 +0800

    Fix: Fix service worker

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 /*jshint esversion: 6 */
 
-const cacheName = 'v1';
-const urlsToCache = [
+const CACHE_VERSION = 'v1';
+const CACHE_FILES = [
   '/',
   'index.html',
   'restaurant.html',
@@ -31,27 +31,71 @@ const urlsToCache = [
 
 self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open(cacheName).then(function(cache) {
+    caches.open(CACHE_VERSION).then(function(cache) {
       console.log('Service worker is caching.');
-      return cache.addAll(urlsToCache);
+      return cache.addAll(CACHE_FILES);
     })
   );
 });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      if (response) {
-        console.log('Found in cache: ', event.request);
-        return response;
-      }
-      else {
-        console.log('Not found in cache. Going to fetch: ', event.request);
-        return fetch(event.request);
-      }
-    })
-    .catch( function(error) {
-      console.log('Error fetching: ', error);
-    })
-  );
+// self.addEventListener('fetch', function(event) {
+//   event.respondWith(
+//     caches.match(event.request).then(function(response) {
+//       if (response) {
+//         console.log('Found in cache: ', event.request);
+//         return response;
+//       }
+//       else {
+//         console.log('Not found in cache. Going to fetch: ', event.request);
+//         return fetch(event.request);
+//       }
+//     })
+//     .catch( function(error) {
+//       console.log('Error fetching: ', error);
+//     })
+//   );
+// });
+
+// Reference: https://www.sitepoint.com/getting-started-with-service-workers/
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request).then(function(res){
+            if(res){
+                console.log('Found in cache: ', event.request);
+                return res;
+            }
+            requestBackend(event);
+        })
+    );
+});
+
+function requestBackend(event){
+    var url = event.request.clone();
+    return fetch(url).then(function(res){
+        //if not a valid response send the error
+        if(!res || res.status !== 200 || res.type !== 'basic'){
+            return res;
+        }
+
+        var response = res.clone();
+
+        caches.open(CACHE_VERSION).then(function(cache){
+            cache.put(event.request, response);
+        });
+
+        return res;
+    });
+}
+
+self.addEventListener('activate', function (event) {
+    event.waitUntil(
+        caches.keys().then(function(keys){
+            return Promise.all(keys.map(function(key, i){
+                if(key !== CACHE_VERSION){
+                    return caches.delete(keys[i]);
+                }
+            }));
+        })
+    );
 });
